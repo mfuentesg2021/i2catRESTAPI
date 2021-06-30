06601356ce5fe2966379dedace3ca6c2aadb2322
mfuentesg2021 <78870976+mfuentesg2021@users.noreply.github.com>
2021-06-30 20:27:37 +0200

    Update python_i2cat.py

--- DockerProject/python_i2cat.py
+++ DockerProject/python_i2cat.py
@@ -5,67 +5,73 @@ Created on Wed Jun  3 20:18:10 2021
 @author: MiguelF
 """
 
-#importing libraries flask and request
-
-from flask import Flask 
+from flask import Flask
 from flask_restful import Api, Resource
 
 import requests
 
-# API Creation
+# API creation
 app = Flask(__name__)
 api = Api(app)
 
-
-
-# 1st endpoint:  “creation_date” and “link” of the 3 first/oldest posts from 31/5/2021
-
-
 class Posts31May2021(Resource):
-      # function to  get resquest with headers order and pagesize included.  
-      def get(self,order,pagesize):
-          response2 = requests.get('https://api.stackexchange.com/2.2/posts?fromdate=1622419200&sort=creation&site=stackoverflow',
-                         headers={"order" :order,"pagesize":pagesize})
-          # json() on response2 to get response3 as a dictionary 
-          response3= response2.json()
+    """ Function to  get request with headers order and pagesize included.
+    First endpoint “creation_date” & “link” of 3 first/oldest post from 31/5/2021
+    """
 
-          # list of dictionaries to obtain the 'creation_date', 'link' from the response2
-          list_of_dicts2 = []
-          for dict_ in response3["items"]:
-              print("keys on dicts_",dict_.keys())
-              res = {key: dict_[key] for key in dict_.keys()
-                               & {'creation_date', 'link'}}
-              list_of_dicts2.append(res)
-          return list_of_dicts2   
+    def get(self, order, pagesize):
+        response2 = requests.get(
+            'https://api.stackexchange.com/2.2/posts?fromdate=1622419200&sort=creation&site=stackoverflow',
+            params={"order": order, "pagesize": pagesize})
+        # Json() on response2 to get response3 as a dictionary
+        response3 = response2.json()
+
+        # List of dictionaries to obtain the 'creation_date',
+        # 'link' from the response2
+        list_of_dicts2 = []
+        for dict_ in response3["items"]:
+            print("keys on dicts_", dict_.keys())
+            res = {key: dict_[key] for key in dict_.keys() &
+                   {'creation_date', 'link'}}
+            list_of_dicts2.append(res)
+
+        return list_of_dicts2
 
 
-#endpoint addition with url routes to complete the order asc or desc and pagezise 10                  
-api.add_resource(Posts31May2021,"/post/<string:order>/<string:pagesize>",endpoint='getposts')
+# Endpoint addition with url routes to complete the order asc
+# or desc and pagesize 10
+api.add_resource(Posts31May2021, "/post/<string:order>/<string:pagesize>",
+                 endpoint='getposts')
 
 
-# 2nd endpoint: The “name” and “award_count” of the 10 first badges 
-# shown when sorting by rank and ordering in ascending manner                  
-                   
+
 class Getbadges(Resource):
-    # function to  get resquest with headers order included.
-    def get(self,order):
-        response2 = requests.get('https://api.stackexchange.com/2.2/badges?pagesize=10&fromdate=1622419200&sort=rank&site=stackoverflow',
-                         headers={"order" :order})
+    """Function to  get request with headers order included.
+    Second endpoint: The “name” and “award_count” of the 10 first badges
+    shown when sorting by rank and ordering in ascending manner.
+    """
+    def get(self, order):
+        response2 = requests.get(
+            'https://api.stackexchange.com/2.2/badges?pagesize=10&fromdate=1622419200&sort=rank&site=stackoverflow',
+            params={"order": order})
         # json() on response2 to get response3 as a dictionary
-        response3= response2.json()
+        response3 = response2.json()
 
-        # list of dictionaries to obtain the 'name', 'award_count' from the response2
+        # List of dictionaries to obtain the 'name', 'award_count'
+        # from the response2
         list_of_dicts = []
         for dict_ in response3["items"]:
-            res = {key: dict_[key] for key in dict_.keys()
-                               & {'name', 'award_count'}}
+            res = {key: dict_[key] for key in dict_.keys() &
+                   {'name', 'award_count'}}
             list_of_dicts.append(res)
-        return list_of_dicts   
 
-# url routes to complete the order asc or desc
-api.add_resource(Getbadges,"/badges/<string:order>",endpoint='getbadges')
+        return list_of_dicts
 
-# running the app including the port=6789 and allowing debug mode 
+
+# Url routes to complete the order asc or desc
+api.add_resource(Getbadges, "/badges/<string:order>", endpoint='getbadges')
+
+# Running the app including the port=6789 and allowing debug mode
 
 if __name__ == "__main__":
-	app.run(debug=True,use_reloader=False,port=6789)  #add use_reloader=False added because of message Restarting with widdowsapi realoader exception 
+    app.run(debug=True, use_reloader=False, port=6789, host='0.0.0.0')  # Add use_reloader=False  because of message Restarting with winddowsapi realoader exception
